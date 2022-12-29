061c4c070eef70c7e464782178f55df72b087f66
Jorrat María Florencia <83671274+jorratmf@users.noreply.github.com>
2022-12-29 04:19:26 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,52 @@
+# Desafio Encriptador de Texto - ALURA LATAM - ORACLE ONE EDUCATION
+
+---
+
+Link del proyecto abajo 👇
+
+---
+
+### Tecnologias utilizadas:
+
+<img src="https://cdn-icons-png.flaticon.com/512/174/174854.png" width='30px' ><img src="https://cdn-icons-png.flaticon.com/512/732/732190.png" width='30px' ><img src="https://cdn-icons-png.flaticon.com/512/5968/5968292.png" width='30px' alt=""><img src="https://cdn.icon-icons.com/icons2/1088/PNG/512/1485282157-adobe-photoshop-raster-graphics-editor-cc-creative-cloud_78285.png" width='33px' >
+
+---
+
+En este primer desafio del curso se nos pidio hacer un codificador de texto usando las siguientes condiciones:
+
+---
+
+Las "llaves" de encriptación que utilizaremos son las siguientes:
+
+`La letra "e" es convertida para "enter"`
+`La letra "i" es convertida para "imes"`
+`La letra "a" es convertida para "ai"`
+`La letra "o" es convertida para "ober"`
+`La letra "u" es convertida para "ufat"`
+
+**Requisitos:**
+
+- Debe funcionar solo con letras minúsculas
+- No deben ser utilizados letras con acentos ni caracteres especiales
+- Debe ser posible convertir una palabra para la versión encriptada también devolver una palabra encriptada para su versión original.
+- la pagina debe estar construida visualmente usando HTML y CSS.
+- La pagina debe ser responsiva (opcional)
+
+Por ejemplo:
+`"gato" => "gaitober"`
+`gaitober" => "gato"`
+
+---
+
+**Caracteristicas:**
+
+- Diseño Responsivo - Mobile UI - Desktop UI
+- Modo claro - Modo oscuro con localstorage
+- Notificaciones de alertas personalizadas
+
+![](https://i.imgur.com/KZjAEGP.png)
+![](https://i.imgur.com/QGFfmv9.png)
+
+---
+
+## [ir a la pagina](https://heric-olier.github.io/Encriptador-de-texto/) 👈
---
+++ js/script.js
@@ -0,0 +1,98 @@
+const alertMessage = document.querySelector(".alert-message-box");
+const textBox = document.querySelector(".text-box");
+const textAlert = document.querySelector(".alert");
+const btnEncript = document.getElementById("encript");
+
+//*! <--------- Function notification alert --------->*/
+
+const notification = (text, img) => {
+  const imgAlert = document.createElement("img");
+  textAlert.textContent = "";
+  textAlert.classList.add("active");
+  setTimeout(() => {
+    textAlert.classList.remove("active");
+  }, 2600);
+  imgAlert.src = img;
+  imgAlert.classList.add("icon-head");
+  textAlert.textContent = text;
+  textAlert.appendChild(imgAlert);
+};
+inputText.focus();
+
+//*! <--------- Function encrypt --------->*/
+function clean() {
+  document.getElementById("inputText").value = "";
+}
+
+function encriptText() {
+  let textResult = document
+    .getElementById("inputText")
+    .value.replace(/e/g, "enter")
+    .replace(/i/g, "imes")
+    .replace(/a/g, "ai")
+    .replace(/o/g, "ober")
+    .replace(/u/g, "ufat");
+
+  if (textResult.length === 0 || /^\s+$/.test(textResult)) {
+    notification("Por favor escriba una palabra", "images/icon-head-lose.svg");
+  } else if (/[^a-z ]/.test(textResult)) {
+    notification(
+      "Solo se permiten letras minúsculas sin acentos",
+      "images/icon-head-lose.svg"
+    );
+  } else {
+    textBox.classList.remove("disabled");
+    alertMessage.textContent = "";
+  }
+
+  document.getElementById("text-result").innerHTML = textResult;
+  inputText.value = "";
+  inputText.focus();
+}
+
+btnEncript.addEventListener("click", encriptText);
+
+//*! <--------- Function decrypt --------->*/
+
+function decryptText() {
+  let textResultDec = document
+    .getElementById("inputText")
+    .value.replace(/enter/g, "e")
+    .replace(/imes/g, "i")
+    .replace(/ai/g, "a")
+    .replace(/ober/g, "o")
+    .replace(/ufat/g, "u");
+
+  document.getElementById("text-result").innerHTML = textResultDec;
+
+  if (textResultDec.length === 0 || /^\s+$/.test(textResultDec)) {
+    notification("Por favor escriba una palabra", "images/icon-head-lose.svg");
+  } else if (/[^a-z ]/.test(textResultDec)) {
+    notification(
+      "Solo se permiten letras minúsculas sin acentos",
+      "images/icon-head-lose.svg"
+    );
+  } else {
+    textBox.classList.remove("disabled");
+    alertMessage.textContent = "";
+  }
+
+  document.getElementById("text-result").innerHTML = textResultDec;
+  inputText.value = "";
+  inputText.focus();
+}
+
+//*! <--------- Function button copy --------->*/
+
+function copyToClipBoard() {
+  let copyText = document.getElementById("text-result").textContent;
+  console.log({ copyText });
+  navigator.clipboard.writeText(copyText).then(() => {
+    if (copyText.length === 0 || /^\s+$/.test(copyText)) {
+      notification("No hay texto para copiar", "images/icon-head-lose.svg");
+    } else {
+      notification("Texto copiado al portapapeles", "images/icon-head-win.svg");
+    }
+  });
+  inputText.focus();
+}
